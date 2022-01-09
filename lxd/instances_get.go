f40f7eca6a75fce74e17f0ce6fd66385e15e948e
Max Asnaashari <max.asnaashari@canonical.com>
2022-01-09 02:04:28 +0000

    lxd/instance: Manually fetch config/devices for projects
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lxd/instances_get.go
+++ lxd/instances_get.go
@@ -272,6 +272,8 @@ func doInstancesGet(d *Daemon, r *http.Request) (interface{}, error) {
 	var result map[string][]string // Containers by node address
 	var nodes map[string]string    // Node names by container
 	filteredProjects := []string{}
+	nodeInstances := map[string]instance.Instance{}
+	mustLoadObjects := recursion > 0 || (recursion == 0 && clauses != nil)
 	err = d.cluster.Transaction(func(tx *db.ClusterTx) error {
 		var err error
 
@@ -302,26 +304,23 @@ func doInstancesGet(d *Daemon, r *http.Request) (interface{}, error) {
 			return err
 		}
 
+		// Get the local instances
+		if mustLoadObjects {
+			for _, project := range filteredProjects {
+				insts, err := instanceLoadNodeProjectAll(d.State(), tx, project, instanceType)
+				if err != nil {
+					return err
+				}
+
+				for _, inst := range insts {
+					nodeInstances[inst.Name()] = inst
+				}
+			}
+		}
 		return nil
 	})
 	if err != nil {
 		return []string{}, err
-	}
-
-	// Get the local instances
-	nodeInstances := map[string]instance.Instance{}
-	mustLoadObjects := recursion > 0 || (recursion == 0 && clauses != nil)
-	if mustLoadObjects {
-		for _, project := range filteredProjects {
-			insts, err := instanceLoadNodeProjectAll(d.State(), project, instanceType)
-			if err != nil {
-				return nil, err
-			}
-
-			for _, inst := range insts {
-				nodeInstances[inst.Name()] = inst
-			}
-		}
 	}
 
 	// Append containers to list and handle errors
